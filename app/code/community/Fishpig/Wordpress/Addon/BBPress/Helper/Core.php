cf02cb8234ef75d177a1d63d2f75941977548ada
Ben Tideswell <ben@fishpig.co.uk>
2018-04-29 08:47:38 +0100

    1.3.0.4

--- app/code/community/Fishpig/Wordpress/Addon/BBPress/Helper/Core.php
+++ app/code/community/Fishpig/Wordpress/Addon/BBPress/Helper/Core.php
@@ -348,6 +348,8 @@ class Fishpig_Wordpress_Addon_BBPress_Helper_Core extends Mage_Core_Helper_Abstr
 	**/
 	protected function _handlePotentialMissingIncludes()
 	{
+		Mage::helper('log');
+
 		Zend_Log::ERR;
 		Zend_Log_Formatter_Simple::DEFAULT_FORMAT;
 
